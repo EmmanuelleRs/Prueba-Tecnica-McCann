86d63a4cba8f214da2d9119da5598972239db032
Emmanuelle Raya <or5311836@gmail.com>
2022-08-04 21:53:32 -0500

    create cars react app

---
+++ apps/cars/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const app = ReactDOM.createRoot(document.querySelector('#react-app'));
+
+app.render(<h1>Hello My name is Emmanuelle </h1>)
